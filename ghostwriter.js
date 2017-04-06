434435a2448ceb341bdef1bf632b7f89bc720f0a
Unknown <oabbott@mail.csuchico.edu>
2017-04-06 11:55:01 -0700

    random tweets

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,17 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["Don't worry about", "Life is about", "When life gets hard", "Sometimes all you need is"];
+  var body = ["the rain", "your doubts", "powerful postitive thoughts", "the future", "a challenge"];
+  var end = ["never give up", "when you fall, get back up"];
+  randIndex = randomUpTo(intro.length - 1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length - 1);
+  sentence = sentence + " " + body[randIndex];
+  randIndex = randomUpTo(end.length - 1);
+  sentence = sentence + " " + end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
